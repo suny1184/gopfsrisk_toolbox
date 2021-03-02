3b796484ad8520673e0f3f3323b91b41b71bee4c
Aaron England <aengland@gopfs.com>
2021-03-01 22:43:30 -0700

    added subsetter class

--- gopfsrisk_toolbox/api.py
+++ gopfsrisk_toolbox/api.py
@@ -1,4 +1,20 @@
 # api
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# define subsetter class
+class Subsetter(BaseEstimator, TransformerMixin):
+	# initialize
+	def __init__(self, list_cols):
+		self.list_cols = list_cols
+	# fit
+	def fit(self, X, y=None):
+		return self
+	# transform
+	def transform(self, X):
+		# subset
+		X = X[self.list_cols]
+		# return
+		return self
 
 # define pipeline class
 class PipelineDataPrep:
