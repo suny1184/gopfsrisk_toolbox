2866fffc6c66225b96da8a7fb10d30f63425dd9e
Aaron England <aengland@gopfs.com>
2021-03-04 14:47:22 -0700

    added another x y split function

--- gopfsrisk_toolbox/leaky_features.py
+++ gopfsrisk_toolbox/leaky_features.py
@@ -14,3 +14,21 @@ def X_Y_SPLIT(df_train, df_valid, logger=None, str_targetname='TARGET__app'):
 		logger.warning('Train and valid dfs split into X and y')
 	# return
 	return df_train, y_train, df_valid, y_valid
+
+# define function for splitting into X and y when also including test data
+def X_Y_SPLIT_WITH_TEST(df_train, df_valid, df_test, logger=None, str_targetname='TARGET__app'):
+	# train
+	y_train = df_train[str_targetname]
+	df_train.drop(str_targetname, axis=1, inplace=True)
+	# valid
+	y_valid = df_valid[str_targetname]
+	df_valid.drop(str_targetname, axis=1, inplace=True)
+	# test
+	y_test = df_test[str_targetname]
+	df_test.drop(str_targetname, axis=1, inplace=True)
+	# if using logger
+	if logger:
+		# log it
+		logger.warning('Train, valid, and test dfs split into X and y')
+	# return
+	return df_train, y_train, df_valid, y_valid, df_test, y_test
