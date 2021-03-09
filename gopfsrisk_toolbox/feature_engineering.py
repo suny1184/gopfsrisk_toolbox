6551af3abf1dc32772c71eeda9559296fc849abe
Aaron England <aengland@gopfs.com>
2021-03-09 10:27:19 -0700

    add feature engineering

---
+++ gopfsrisk_toolbox/feature_engineering.py
@@ -0,0 +1,9 @@
+# feature engineering (for API)
+
+# create fe class
+class FeatureEngineeringAaron:
+    # transform
+    def transform(X):
+        X[] = X[] / X[]
+        # return
+        return X
