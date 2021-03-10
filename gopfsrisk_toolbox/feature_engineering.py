e8af85ce3530281d9cc58aadba432a5d711e62a6
Aaron England <aengland@gopfs.com>
2021-03-09 18:30:04 -0700

    fe

--- gopfsrisk_toolbox/feature_engineering.py
+++ gopfsrisk_toolbox/feature_engineering.py
@@ -2,11 +2,11 @@
 
 # create fe class
 class FeatureEngineeringAaron:
-    # transform
-    def transform(X):
-    	try:
-        	X['debt_to_income'] = X['fltBalanceCurrent__debt_sum'] / X['fltGrossMonthly__income_sum']
-        except:
-        	pass
-        # return
-        return X
+	# transform
+	def transform(X):
+		try:
+			X['debt_to_income'] = X['fltBalanceCurrent__debt_sum'] / X['fltGrossMonthly__income_sum']
+		except:
+			pass
+		# return
+		return X
