1b27c2d18ff636d6cd820ae764d9f0e9671f02ec
James <64863134+jqiu7610@users.noreply.github.com>
2021-06-08 15:24:53 -0600

    copy from JQ toolbox

--- gopfsrisk_toolbox/feature_engineering.py
+++ gopfsrisk_toolbox/feature_engineering.py
@@ -641,7 +641,7 @@ class FeatureEngineeringJQ:
 		
 		#income and tradeline info
 		# 32s 33s 34s 35s max, total, utilization, average verified 12 months vs income
-		# ================================== trades ==============================================
+		# ================================== trades over income ==============================================
 		try:
 			X['eng_at32s_to_income'] = X['at32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -670,8 +670,37 @@ class FeatureEngineeringJQ:
 			X['eng_at35b_to_income'] = X['at35b__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
 			pass
-		
-		#================================= auto trades ========================================
+		# ============================== income over trades ============================================
+		try:
+			X['eng_income_to_at32s'] = X['fltGrossMonthly__income_sum'] / X['at32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_at33a'] = X['fltGrossMonthly__income_sum'] / X['at33a__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_at33b'] = X['fltGrossMonthly__income_sum'] / X['at33b__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_at34a'] = X['fltGrossMonthly__income_sum'] / X['at34a__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_at34b'] = X['fltGrossMonthly__income_sum'] / X['at34b__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_at35a'] = X['fltGrossMonthly__income_sum'] / X['at35a__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_at35b'] = X['fltGrossMonthly__income_sum'] / X['at35b__tuaccept']
+		except:
+			pass
+
+		#================================= auto trades over income ========================================
 		try:
 			X['eng_au32s_to_income'] = X['au32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -688,8 +717,26 @@ class FeatureEngineeringJQ:
 			X['eng_au35s_to_income'] = X['au35s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
 			pass
+
+		# =============================== income over auto trades =========================================
+		try:
+			X['eng_income_to_au32s'] = X['fltGrossMonthly__income_sum'] / X['au32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_au33s'] = X['fltGrossMonthly__income_sum'] / X['au33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_au34s'] = X['fltGrossMonthly__income_sum'] / X['au34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_au35s'] = X['fltGrossMonthly__income_sum'] / X['au35s__tuaccept']
+		except:
+			pass		
 		
-		#=============================== credit cards =========================================
+		#=============================== credit cards over income =========================================
 		try:
 			X['eng_bc32s_to_income'] = X['bc32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -706,8 +753,26 @@ class FeatureEngineeringJQ:
 			X['eng_bc35s_to_income'] = X['bc35s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
 			pass
-		
-		#=============================== bank installments ====================================
+		# ============================== income over credit cards =========================================
+		try:
+			X['eng_income_to_bc32s'] = X['fltGrossMonthly__income_sum'] / X['bc32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_bc33s'] = X['fltGrossMonthly__income_sum'] / X['bc33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_bc34s'] = X['fltGrossMonthly__income_sum'] / X['bc34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_bc35s'] = X['fltGrossMonthly__income_sum'] / X['bc35s__tuaccept']
+		except:
+			pass
+
+
+		#=============================== bank installments over income====================================
 		try:
 			X['eng_bi32s_to_income'] = X['bi32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -725,7 +790,25 @@ class FeatureEngineeringJQ:
 		except:
 			pass
 		
-		#============================= bank revolvers ========================================
+		#============================= income over bank installments ====================================
+		try:
+			X['eng_income_to_bi32s'] = X['fltGrossMonthly__income_sum'] / X['bi32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_bi33s'] = X['fltGrossMonthly__income_sum'] / X['bi33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_bi34s'] = X['fltGrossMonthly__income_sum'] / X['bi34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_bi35s'] = X['fltGrossMonthly__income_sum'] / X['bi35s__tuaccept']
+		except:
+			pass
+
+		#============================= bank revolvers over income========================================
 		try:
 			X['eng_br32s_to_income'] = X['br32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -743,7 +826,25 @@ class FeatureEngineeringJQ:
 		except:
 			pass
 		
-		#============================ finance installments ===================================
+		#============================ income over bank revolvers ========================================
+		try:
+			X['eng_income_to_br32s'] = X['fltGrossMonthly__income_sum'] / X['br32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_br33s'] = X['fltGrossMonthly__income_sum'] / X['br33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_br34s'] = X['fltGrossMonthly__income_sum'] / X['br34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_br35s'] = X['fltGrossMonthly__income_sum'] / X['br35s__tuaccept']
+		except:
+			pass
+
+		#============================ finance installments over income===================================
 		try:
 			X['eng_fi32s_to_income'] = X['fi32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -761,7 +862,25 @@ class FeatureEngineeringJQ:
 		except:
 			pass
 		
-		#============================ finance revolvers ======================================
+		# =========================== income over finance installments ==================================
+		try:
+			X['eng_income_to_fi32s'] = X['fltGrossMonthly__income_sum'] / X['fi32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_fi33s'] = X['fltGrossMonthly__income_sum'] / X['fi33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_fi34s'] = X['fltGrossMonthly__income_sum'] / X['fi34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_fi35s'] = X['fltGrossMonthly__income_sum'] / X['fi35s__tuaccept']
+		except:
+			pass
+
+		#============================ finance revolvers over income======================================
 		try:
 			X['eng_fr32s_to_income'] = X['fr32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -778,8 +897,25 @@ class FeatureEngineeringJQ:
 			X['eng_fr35s_to_income'] = X['fr35s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
 			pass
-		
-		#============================ home equity ============================================
+		#============================ income over finance revolvers =====================================
+		try:
+			X['eng_income_to_fr32s'] = X['fltGrossMonthly__income_sum'] / X['fr32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_fr33s'] = X['fltGrossMonthly__income_sum'] / X['fr33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_fr34s'] = X['fltGrossMonthly__income_sum'] / X['fr34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_fr35s'] = X['fltGrossMonthly__income_sum'] / X['fr35s__tuaccept']
+		except:
+			pass
+
+		#============================ home equity over income============================================
 		try:
 			X['eng_hi32s_to_income'] = X['hi32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -797,7 +933,25 @@ class FeatureEngineeringJQ:
 		except:
 			pass
 		
-		#========================== HE LOC ==================================================
+		#=============================income over home equity===========================================
+		try:
+			X['eng_income_to_hi32s'] = X['fltGrossMonthly__income_sum'] / X['hi32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_hi33s'] = X['fltGrossMonthly__income_sum'] / X['hi33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_hi34s'] = X['fltGrossMonthly__income_sum'] / X['hi34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_hi35s'] = X['fltGrossMonthly__income_sum'] / X['hi35s__tuaccept']
+		except:
+			pass
+
+		#========================== HE LOC over income==================================================
 		try:
 			X['eng_hr32s_to_income'] = X['hr32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -815,7 +969,25 @@ class FeatureEngineeringJQ:
 # 		except:
 # 			pass
 		
-		#============================ installment trades =====================================
+		#============================ income over HE LOC ===============================================
+		try:
+			X['eng_income_to_hr32s'] = X['fltGrossMonthly__income_sum'] / X['hr32s__tuaccept']
+		except:
+			pass
+# 		try:
+# 			X['eng_income_to_hr33s'] = X['fltGrossMonthly__income_sum'] / X['hr33s__tuaccept']
+# 		except:
+# 			pass
+		try:
+			X['eng_income_to_hr34s'] = X['fltGrossMonthly__income_sum'] / X['hr34s__tuaccept']
+		except:
+			pass
+# 		try:
+# 			X['eng_income_to_hr35s'] = X['fltGrossMonthly__income_sum'] / X['hr35s__tuaccept']
+# 		except:
+# 			pass
+
+		#============================ installment trades over income=====================================
 		try:
 			X['eng_in32s_to_income'] = X['in32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -833,7 +1005,26 @@ class FeatureEngineeringJQ:
 		except:
 			pass
 		
-		#=========================== mortgage trades =========================================
+		#=========================== income over installment trades =====================================
+		try:
+			X['eng_income_to_in32s'] = X['fltGrossMonthly__income_sum'] / X['in32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_in33s'] = X['fltGrossMonthly__income_sum'] / X['in33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_in34s'] = X['fltGrossMonthly__income_sum'] / X['in34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_in35s'] = X['fltGrossMonthly__income_sum'] / X['in35s__tuaccept']
+		except:
+			pass
+		
+
+		#=========================== mortgage trades over income=========================================
 		try:
 			X['eng_mt32s_to_income'] = X['mt32s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
@@ -850,6 +1041,25 @@ class FeatureEngineeringJQ:
 			X['eng_mt35s_to_income'] = X['mt35s__tuaccept'] / X['fltGrossMonthly__income_sum']
 		except:
 			pass
+
+		#============================ income over mortgage trades ========================================
+		try:
+			X['eng_income_to_mt32s'] = X['fltGrossMonthly__income_sum'] / X['mt32s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_mt33s'] = X['fltGrossMonthly__income_sum'] / X['mt33s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_mt34s'] = X['fltGrossMonthly__income_sum'] / X['mt34s__tuaccept']
+		except:
+			pass
+		try:
+			X['eng_income_to_mt35s'] = X['fltGrossMonthly__income_sum'] / X['mt35s__tuaccept']
+		except:
+			pass
+
 		#======================== Bankruptcy fields =========================================
 		#bankruptcy 24 months vs bankruptcy
 		try:
@@ -881,13 +1091,74 @@ class FeatureEngineeringJQ:
 			X['eng_g099a_to_at27s'] = X['g099a__tuaccept'] / X['at27s__tuaccept']
 		except:
 			pass
+
 		# BK fields
-		# X['g094s__tuaccept'] number of public record bankruptcies
-		# X['g099s__tuaccept'] number of public BK past 24 months
-		# X['g100s__tuaccept'] number of tradeline BK
-				
+		# --DONE--X['g094s__tuaccept'] number of public record bankruptcies
+		# --DONE--X['g099s__tuaccept'] number of public BK past 24 months
+		# --DONE--X['g100s__tuaccept'] number of tradeline BK
+		try:
+			X['eng_inttype_times_linkf032'] = X['intType__app'] * X['LINKF032__tucvlink']
+		except:
+			pass
+		# months since most recent inquiry over months since most recent public record BK
+		try:
+			X['eng_g102s_to_s207s'] = X['g102s__tuaccept'] / X['s207s__tuaccept']
+		except:
+			pass
+		# months since most recent inquiry over months since most recent tradeline BK
+		try:
+			X['eng_g102s_to_s207a'] = X['g102s__tuaccept'] / X['s207a__tuaccept']
+		except:
+			pass
+		# =====================BK between CREDIT VISION VS FACTOR TRUST======================
+		# public record BK 24 months
+		try:
+			X['eng_linkt008_times_g099s'] = X['LINKT008__tucvlink'] * X['g099s__tuaccept']
+		except:
+			pass
+		# trade line BK
+		try:
+			X['eng_linkt009_times_g100s'] = X['LINKT009__tucvlink'] * X['g100s__tuaccept']
+		except:
+			pass
+		# public record BK	
+		try:
+			X['eng_linkt010_times_g094s'] = X['LINKT010__tucvlink'] * X['g094s__tuaccept']
+		except:
+			pass
 		# return
 		return X
+
+list_cols_eng_raw = ['fltAmountFinanced__app', 'fltApprovedPriceWholesale__app', 'jt03s__tuaccept',
+					 'fltGrossMonthly__income_sum', 'fltApprovedDownTotal__app', 'fltAmountFinanced__app', 'fltApprovedDownTotal__app', 
+					 'fltGrossMonthly__income_sum', 'fltGrossMonthly__income_std', 'fltGrossMonthly__income_min', 'fltGrossMonthly__income_median',
+					 'fltGrossMonthly__income_mean', 'fltGrossMonthly__income_max', 'fltApprovedDownTotal__app', 'fltApprovedPriceWholesale__app', 'at02s__tuaccept', 
+					 'at01s__tuaccept', 'at03s__tuaccept', 'at01s__tuaccept', 'at09s__tuaccept', 'at27s__tuaccept', 'at32s__tuaccept', 
+					 'at57s__tuaccept', 'at33a__tuaccept', 'at33b__tuaccept', 'at34a__tuaccept', 'at34b__tuaccept', 'at35a__tuaccept',
+					 'at35b__tuaccept', 'at101s__tuaccept', 'au02s__tuaccept', 'au01s__tuaccept', 'au03s__tuaccept', 'au34s__tuaccept', 
+					 'au35s__tuaccept', 'au09s__tuaccept', 'au21s__tuaccept', 'au20s__tuaccept', 'au27s__tuaccept', 'au32s__tuaccept',
+					 'au33s__tuaccept', 'bc02s__tuaccept', 'bc01s__tuaccept', 'bi03s__tuaccept', 'bc03s__tuaccept', 'bc09s__tuaccept', 
+					 'bc21s__tuaccept', 'bc20s__tuaccept', 'bc27s__tuaccept', 'bc32s__tuaccept', 'bc33s__tuaccept', 'bc34s__tuaccept', 
+					 'bc35s__tuaccept', 'bi02s__tuaccept', 'bi01s__tuaccept', 'bi12s__tuaccept', 'bi21s__tuaccept', 'bi20s__tuaccept', 
+					 'bi32s__tuaccept', 'bi34s__tuaccept', 'bi33s__tuaccept', 'bi35s__tuaccept', 'br02s__tuaccept', 'br01s__tuaccept', 
+					 'br03s__tuaccept', 'br09s__tuaccept', 'br21s__tuaccept', 'br20s__tuaccept', 'br27s__tuaccept', 'br32s__tuaccept', 
+					 'br33s__tuaccept', 'br34s__tuaccept', 'br35s__tuaccept', 'co03s__tuaccept', 'co01s__tuaccept', 'co07s__tuaccept', 
+					 'co05s__tuaccept', 'fc03s__tuaccept', 'fc01s__tuaccept', 'fc07s__tuaccept', 'fc05s__tuaccept', 'fi02s__tuaccept', 
+					 'fi01s__tuaccept', 'fi03s__tuaccept', 'fi01s__tuaccept', 'fi09s__tuaccept', 'fi02s__tuaccept', 'fi09s__tuaccept', 
+					 'fi21s__tuaccept', 'fi20s__tuaccept', 'fi27s__tuaccept', 'fi01s__tuaccept', 'fr02s__tuaccept', 'fr01s__tuaccept', 
+					 'fr03s__tuaccept', 'fr09s__tuaccept', 'fr32s__tuaccept', 'fr33s__tuaccept', 'fr34s__tuaccept', 'fr35s__tuaccept', 
+					 'fi32s__tuaccept', 'fi33s__tuaccept','fi34s__tuaccept', 'fi35s__tuaccept', 'hi02s__tuaccept', 'hi01s__tuaccept', 
+					 'hi03s__tuaccept', 'hi01s__tuaccept', 'hi09s__tuaccept', 'hi21s__tuaccept', 'hi20s__tuaccept', 'hi27s__tuaccept', 
+					 'hi32s__tuaccept', 'hi33s__tuaccept','hi34s__tuaccept', 'hi35s__tuaccept','hr02s__tuaccept', 'hr01s__tuaccept', 
+					 'hr03s__tuaccept', 'hr12s__tuaccept', 'hr01s__tuaccept', 'hr21s__tuaccept', 'hr20s__tuaccept', 'hr32s__tuaccept', 
+					 'hr34s__tuaccept', 'in02s__tuaccept', 'in01s__tuaccept', 'in03s__tuaccept', 'g100s__tuaccept', 'g099a__tuaccept'
+					 'in09s__tuaccept', 'in12s__tuaccept', 'in21s__tuaccept', 'in20s__tuaccept', 'in27s__tuaccept', 'in28s__tuaccept', 
+					 'in32s__tuaccept', 'in33s__tuaccept', 'in34s__tuaccept', 'in35s__tuaccept', 'lm08s__tuaccept', 'lm01s__tuaccept', 
+					 'lm25s__tuaccept', 'mt02s__tuaccept', 'mt01s__tuaccept', 'mt03s__tuaccept', 'mt09s__tuaccept', 'mt12s__tuaccept', 
+					 'mt21s__tuaccept', 'mt20s__tuaccept', 'mt27s__tuaccept', 'mt01s__tuaccept', 'mt32s__tuaccept', 'mt33s__tuaccept', 
+					 'mt34s__tuaccept', 'mt35s__tuaccept', 'g099s__tuaccept', 'intType__app', 'LINKF032__tucvlink', 'LINKT008__tucvlink',
+					 'LINKT009__tucvlink', 'LINKT010__tucvlink', 'g094s__tuaccept']
+
 
 class FeatureBucketingJQ:
 	# init
