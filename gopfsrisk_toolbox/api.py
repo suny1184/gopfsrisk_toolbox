931af5c5c24f98c1e09a269f2243aa1e54e2eb38
Aaron England <aengland@gopfs.com>
2021-08-24 15:01:21 -0600

    added bool_aa option

--- gopfsrisk_toolbox/api.py
+++ gopfsrisk_toolbox/api.py
@@ -107,50 +107,6 @@ class PipelineDataPrep:
 		# return
 		return y_hat_mean
 
-# define pipeline class
-class PipelineDataPrepEnsemble:
-	# initialize
-	def __init__(self, list_transformers, model_a, model_b, bool_classifier=True):
-		self.list_transformers = list_transformers
-		self.model = model
-		self.bool_classifier = bool_classifier
-	# prep predict
-	def prep_predict(self, X, bool_lower=True):
-		# loop through transformers
-		time_start = time.perf_counter()
-		for transformer in self.list_transformers:
-			# transform
-			X = transformer.transform(X)
-		print(f'Time to transform: {(time.perf_counter()-time_start):0.5} sec.')
-		# logic
-		if bool_lower:
-			# make all cols lower
-			X.columns = X.columns.str.lower()
-		# save to object
-		self.X = X
-		# logic
-		time_start = time.perf_counter()
-		# if using a classifier
-		if self.bool_classifier:
-			# model_a predictions
-			predictions_a = model_a.predict_proba(X[self.model_a.feature_names_])[:,1]
-			# model_b predictions
-			predictions_b = model_b.predict_proba(X[self.model_b.feature_names_])[:,1]
-		else:
-			# model_a predictions
-			predictions_a = model_a.predict(X[self.model_a.feature_names_])
-			# model_b predictions
-			predictions_b = model_b.predict(X[self.model_b.feature_names_])
-		# ensemble
-		y_hat = (predictions_a + predictions_b) / 2
-		# save to object
-		self.y_hat = y_hat
-		# get mean
-		y_hat_mean = np.mean(y_hat)
-		print(f'Time to predict: {(time.perf_counter()-time_start):0.5} sec.')
-		# return
-		return y_hat_mean
-
 # define function for payload parsing and generating output
 class ParsePayload:
 	# initialize
@@ -169,18 +125,12 @@ class ParsePayload:
 					   list_string_cols,
 					   dict_aa_pd,
 					   dict_tiers,
-					   bool_debt=False,
-					   list_feats_raw_debt=None,
-					   list_feats_agg_debt=None,
-					   dict_debt_agg=None):
+					   bool_aa):
 		# args
 		self.list_feats_raw_app = list_feats_raw_app
 		self.list_feats_raw_inc = list_feats_raw_inc
 		self.list_feats_agg_inc = list_feats_agg_inc
 		self.dict_income_agg = dict_income_agg
-		self.list_feats_raw_debt = list_feats_raw_debt
-		self.list_feats_agg_debt = list_feats_agg_debt
-		self.dict_debt_agg = dict_debt_agg
 		self.list_feats_raw_ln = list_feats_raw_ln
 		self.list_feats_raw_tuaccept = list_feats_raw_tuaccept
 		self.list_feats_raw_cvlink = list_feats_raw_cvlink
@@ -192,7 +142,7 @@ class ParsePayload:
 		self.list_non_numeric_pd = list_non_numeric_pd
 		self.dict_aa_pd = dict_aa_pd
 		self.dict_tiers = dict_tiers
-		self.bool_debt = bool_debt
+		self.bool_aa = bool_aa
 	# payload for each applicant
 	def get_payload_df(self, json_str_request):
 		# get the payload for each applicant
@@ -276,48 +226,6 @@ class ParsePayload:
 		self.df_inc = df_inc
 		# return object
 		return self	
-	# define parse_debt
-	def parse_debt(self, str_values):
-		# create error
-		self.error_debt = ''
-		# put into df
-		df_debt = pd.read_csv(StringIO(str_values), delimiter=',', usecols=lambda col: col.lower() in self.list_feats_raw_debt)
-		# convert to lower
-		df_debt.columns = df_debt.columns.str.lower()
-		# check if df is empty
-		if df_debt.empty:
-			# create error
-			self.error_debt = 'No debt data'
-			# create empty df with list_feats_debt_agg as cols
-			df_debt = pd.DataFrame(columns=self.list_feats_agg_debt)
-			# create row with nan
-			df_debt = df_debt.append(pd.Series(), ignore_index=True)
-		else:
-			# filter rows
-			df_debt = df_debt[(df_debt['bitinvalid']==False) & (df_debt['bituse']==True)]
-			# if df_debt is empty after filtering on bit cols
-			if df_debt.empty:
-				# create errors
-				self.error_debt = 'No debt data due to bit filter'
-				# create empty df with list_feats_debt_agg as cols
-				df_debt = pd.DataFrame(columns=self.list_feats_agg_debt)
-				# create row with nan
-				df_debt = df_debt.append(pd.Series(), ignore_index=True)
-			else:
-				# append __debt to each column name
-				df_debt.columns = [f'{col}__debt' for col in df_debt.columns]
-				# aggregate
-				df_debt = df_debt.groupby('uniqueid__debt', as_index=False).agg(self.dict_debt_agg)
-				# rename columns
-				df_debt.columns = [f'{tpl_col[0]}_{tpl_col[1]}' for tpl_col in list(df_debt.columns)]
-		# make sure bottom row is selected
-		df_debt = pd.DataFrame(df_debt.iloc[-1, :]).T
-		# reset index
-		df_debt.reset_index(drop=True, inplace=True)
-		# save to object
-		self.df_debt = df_debt
-		# return object
-		return self
 	# define parse_ln
 	def parse_ln(self, str_values):
 		# create error
@@ -455,13 +363,6 @@ class ParsePayload:
 					# append 
 					list_errors.append(self.error_inc)
 					list_df.append(self.df_inc)
-				# debt
-				if (dict_data['name'] == 'Debts') and (self.bool_debt):
-					# parse debt
-					self.parse_debt(str_values=str_values)
-					# append 
-					list_errors.append(self.error_debt)
-					list_df.append(self.df_debt)
 				# ln
 				if dict_data['name'] == 'Lexis Nexis Risk View 5':
 					# parse ln
@@ -699,7 +600,7 @@ class ParsePayload:
 		# time
 		flt_sec_counter = time.perf_counter()-time_start
 		self.flt_sec_counter = flt_sec_counter
-		print(f'Time to generate counter-offers: {flt_sec_counter:0.5} sec.')
+		print(f'Time to generate predictions: {flt_sec_counter:0.5} sec.')
 		# return object
 		return self
 	# define adverse_action
@@ -740,18 +641,35 @@ class ParsePayload:
 		return self
 	# define generate output
 	def generate_output(self, json_str_request):
-		# get adverse action
-		self.adverse_action(json_str_request=json_str_request)
-		# create df
-		time_start = time.perf_counter()
-		# create df
-		df_output = pd.DataFrame({'Row_id': self.list_unique_id,
-								  'Score_pd': self.y_hat_pd,
-								  'Score_lgd': self.y_hat_lgd,
-							      'Score_ecnl': [self.y_hat_pd_x_lgd for id_ in self.list_unique_id],
-							      'Score_ecnl_mod': [self.y_hat_pd_x_lgd_mod for id_ in self.list_unique_id],
-								  'Key_factors': self.list_list_reasons,
-								  'Outlier_score': [0.0 for id_ in self.list_unique_id]})
+		# if getting AA (slow)
+		if self.bool_aa:
+			# get adverse action, which gets counter_offers
+			self.adverse_action(json_str_request=json_str_request)
+			# start time
+			time_start = time.perf_counter()
+			# create df
+			df_output = pd.DataFrame({'Row_id': self.list_unique_id,
+									  'Score_pd': self.y_hat_pd,
+									  'Score_lgd': self.y_hat_lgd,
+								      'Score_ecnl': [self.y_hat_pd_x_lgd for id_ in self.list_unique_id],
+								      'Score_ecnl_mod': [self.y_hat_pd_x_lgd_mod for id_ in self.list_unique_id],
+									  'Key_factors': self.list_list_reasons,
+									  'Outlier_score': [0.0 for id_ in self.list_unique_id]})
+		# if not getting AA (fast)
+		else:
+			# generate predictions 
+			self.counter_offers(json_str_request=json_str_request, int_n_samples=100)
+			# start time
+			time_start = time.perf_counter()
+			# create df
+			df_output = pd.DataFrame({'Row_id': self.list_unique_id,
+									  'Score_pd': self.y_hat_pd,
+									  'Score_lgd': self.y_hat_lgd,
+								      'Score_ecnl': [self.y_hat_pd_x_lgd for id_ in self.list_unique_id],
+								      'Score_ecnl_mod': [self.y_hat_pd_x_lgd_mod for id_ in self.list_unique_id],
+									  'Key_factors': [[] for id_ in self.list_unique_id],
+									  'Outlier_score': [0.0 for id_ in self.list_unique_id]})
+
 		# convert to json
 		str_output_ = df_output.to_json(orient='records')
 		# convert to list
@@ -760,26 +678,36 @@ class ParsePayload:
 		list_errors_final = list(chain(*self.list_list_errors))
 		# remove empty strings from list_errors_final
 		list_errors_final = [err for err in list_errors_final if err != '']
-		# subset to cols we want
-		X_lg_grouped_sub = self.X_lg_grouped[['fltapproveddowntotal__app',
-										 	  'fltamountfinanced__app',
-										 	  'fltapprovedpricewholesale__app',
-										 	  'ecnl_mod',
-										 	  'Tier']]
-		# rename columns
-		X_lg_grouped_sub.columns = ['Cash Down',
-									'Amount Financed',
-									'Price Wholesale',
-									'ECNL Modified',
-									'Tier']
-		# create final output
-		output_final = {"Request_id": "",
-				        "Zaml_processing_id": "",
-						"Response": [{"Model_name":"prestige-GenXI",
-					                  "Model_version":"v2",
-									  "Results":list_output,
-									  "Errors":list_errors_final,
-									  "Counter-Offers":X_lg_grouped_sub.to_dict('records')}]}
+
+		# if getting AA (slow)
+		if self.bool_aa:
+			# subset to cols we want
+			X_lg_grouped_sub = self.X_lg_grouped[['fltapproveddowntotal__app',
+											 	  'fltamountfinanced__app',
+											 	  'fltapprovedpricewholesale__app',
+											 	  'ecnl_mod']]
+			# rename columns
+			X_lg_grouped_sub.columns = ['Cash Down',
+										'Amount Financed',
+										'Price Wholesale',
+										'ECNL Modified']
+			# create final output
+			output_final = {"Request_id": "",
+					        "Zaml_processing_id": "",
+							"Response": [{"Model_name":"prestige-GenXI",
+						                  "Model_version":"v2",
+										  "Results":list_output,
+										  "Errors":list_errors_final,
+										  "Counter-Offers":X_lg_grouped_sub.to_dict('records')}]}
+		# if not getting AA (fast)
+		else:
+			# create final output
+			output_final = {"Request_id": "",
+					        "Zaml_processing_id": "",
+							"Response": [{"Model_name":"prestige-GenXI",
+						                  "Model_version":"v2",
+										  "Results":list_output,
+										  "Errors":list_errors_final}]}
 		# save to object
 		self.output_final = output_final
 		# time
